e777a9db010bc4250aa23c7690670d633969b509
Lothean <contact@lothean.net>
2017-11-22 22:49:27 +0100

    First working version
    Only 4 buttons are working with a nice fade effect : ON/OFF, DIY1, DIY2, DIY3 (which are predefined colors, you can change them in the code)

--- RGBW-Controller.ino
+++ RGBW-Controller.ino
@@ -1,8 +1,16 @@
 #include <IRremote.h>
-#include <IRremoteInt.h>
+#define IR_ON    0xFF02FD
+#define IR_DIY1   0xFF30CF
+#define IR_DIY2   0xFFB04F
+#define IR_DIY3   0xFF708F
+bool isOn = true;
 int irSensor = 12;
 byte rgbw[] = {5, 9, 6, 10}; //change the pins here according to yours
-byte currentColor[] = {255, 255, 255, 128}; // The current color, stored to make the fade effect
+byte currentColor[] = {0, 0, 0, 0}; // The current color, stored to make the fade effect
+byte newColor[] = {255, 255, 255, 128};
+byte backupColor[] = {255, 255, 255, 128};
+IRrecv irrecv(irSensor);
+decode_results results;
 void setup() {
   for (int i = 2; i <= 13; i++) {
     pinMode(i, OUTPUT);
@@ -10,10 +18,14 @@ void setup() {
   pinMode(irSensor, INPUT_PULLUP);
   bitSet(TCCR1B, WGM12); //enables fast PWM on ATmega 328P (might be different on others µC)
   irrecv.enableIRIn();
+  state();
+  byte newColor[] = {255, 255, 255, 128};
+  fadeUpdate();
 }
 
 void loop() {
-  fadeUpdate(currentColor);
+  ir();
+  delay(15);
 }
 
 void refresh () {
@@ -22,8 +34,8 @@ void refresh () {
   }
 }
 
-void fadeUpdate (byte newColor[]) {
-  while (currentColor != newColor) {
+void fadeUpdate () {
+  while (currentColor[0] != newColor[0] || currentColor[1] != newColor[1] || currentColor[2] != newColor[2] || currentColor[3] != newColor[3]) {
     for (int i = 0; i <= 3; i++) {
       if (currentColor[i] < newColor[i]) {
         currentColor[i]++;
@@ -33,6 +45,59 @@ void fadeUpdate (byte newColor[]) {
       }
       analogWrite(rgbw[i], currentColor[i]);
     }
+    delay(5);
+  }
+}
+void ir() {
+
+  if (irrecv.decode(&results)) {
+    if (results.value == IR_ON) {
+      isOn = !isOn;
+      state();
+    }
+    if (results.value == IR_DIY1 && isOn) {
+      newColor[0] = 255;
+      newColor[1] = 255;
+      newColor[2] = 255;
+      newColor[3] = 128;
+      fadeUpdate();
+    }
+    if (results.value == IR_DIY2 && isOn) {
+      newColor[0] = 1;
+      newColor[1] = 1;
+      newColor[2] = 1;
+      newColor[3] = 1;
+      fadeUpdate();
+    }
+    if (results.value == IR_DIY3 && isOn) {
+      newColor[0] = 1;
+      newColor[1] = 0;
+      newColor[2] = 0;
+      newColor[3] = 0;
+      fadeUpdate();
+    }
+    irrecv.resume();
   }
 }
 
+void state() {
+  if (isOn) {
+    for (int i = 0; i <= 3; i++) {
+      newColor[i] = backupColor[i];
+    }
+    digitalWrite(13, HIGH);
+    fadeUpdate();
+
+  }
+  else {
+    for (int i = 0; i <= 3; i++) {
+      backupColor[i] = currentColor[i];
+    }
+    newColor[0] = 0;
+    newColor[1] = 0;
+    newColor[2] = 0;
+    newColor[3] = 0;
+    fadeUpdate();
+    digitalWrite(13, LOW);
+  }
+}
