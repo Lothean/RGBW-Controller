b1d98a07d545ef6a296ba040dfd9a8c7736aed7f
Lothean <contact@lothean.net>
2017-11-23 10:46:13 +0100

    Commented RGBW-Controller.ino
    I know reading code is harder than writing it.

--- RGBW-Controller.ino
+++ RGBW-Controller.ino
@@ -1,5 +1,5 @@
 #include <IRremote.h>
-#define IR_ON    0xFF02FD
+#define IR_ON    0xFF02FD         //IR codes of your remote, here, I've used the codes of the standard RGB LED strip controller remote
 #define IR_DIY1   0xFF30CF
 #define IR_DIY2   0xFFB04F
 #define IR_DIY3   0xFF708F
@@ -7,39 +7,39 @@
 #define IR_DIY5   0xFF906F 
 #define IR_DIY6   0xFF50AF
 bool isOn = true;
-int irSensor = 12;
-byte rgbw[] = {5, 9, 6, 10}; //change the pins here according to yours
-byte currentColor[] = {0, 0, 0, 0}; // The current color, stored to make the fade effect
-byte newColor[] = {255, 255, 255, 128};
-byte backupColor[] = {255, 255, 255, 128};
+int irSensor = 12; // The input pin for your IR sensor 
+byte rgbw[] = {5, 9, 6, 10}; //change the pins here according to yours in this order (Red, Green, Blue, White)
+byte currentColor[] = {0, 0, 0, 0}; // The current color, stored to make the fade effect (set to zero at first to make the fade effect at first launch)
+byte newColor[] = {255, 255, 255, 128}; // The target color, stored to make the fade effect
+byte backupColor[] = {255, 255, 255, 128}; // Backup of the old current color, used to make the ON/OFF fade effect
 IRrecv irrecv(irSensor);
 decode_results results;
 void setup() {
-  for (int i = 2; i <= 13; i++) {
+  for (int i = 2; i <= 13; i++) { // We define all pins from 2 to 13 as outputs, it's not very clean but hey it works
     pinMode(i, OUTPUT);
   }
-  pinMode(irSensor, INPUT_PULLUP);
+  pinMode(irSensor, INPUT_PULLUP); // Since the IR sensor requires a pull-up resistor on its output, we use the internal one (Might not work with your µC, tested with 328P)
   bitSet(TCCR1B, WGM12); //enables fast PWM on ATmega 328P (might be different on others µC)
   irrecv.enableIRIn();
   state();
-  byte newColor[] = {255, 255, 255, 128};
-  fadeUpdate();
+  byte newColor[] = {255, 255, 255, 128}; // The first color to come up on the Arduino start
+  fadeUpdate(); // Apply the color
 }
 
 void loop() {
-  ir();
+  ir(); // This function is called everytime in the loop to handle IR signals and use them
   delay(15);
 }
 
-void refresh () {
+void refresh () { // Not used at the moment, but might be useful
   for (int i = 0; i <= 3; i++) {
     analogWrite(rgbw[i], currentColor[i]);
   }
 }
 
-void fadeUpdate () {
-  while (currentColor[0] != newColor[0] || currentColor[1] != newColor[1] || currentColor[2] != newColor[2] || currentColor[3] != newColor[3]) {
-    for (int i = 0; i <= 3; i++) {
+void fadeUpdate () { // This function takes the color in the global variable newColor and apply it with a crossfade effect based on the currentColor
+  while (currentColor[0] != newColor[0] || currentColor[1] != newColor[1] || currentColor[2] != newColor[2] || currentColor[3] != newColor[3]) { // While both colors are not the same, dirty code, looking for improvements
+    for (int i = 0; i <= 3; i++) {        // We check through the colors with a for loop, compare them, increase or decrease them and apply the color to make the fade effect
       if (currentColor[i] < newColor[i]) {
         currentColor[i]++;
       }
@@ -48,17 +48,17 @@ void fadeUpdate () {
       }
       analogWrite(rgbw[i], currentColor[i]);
     }
-    delay(5);
+    delay(5); // Adjust this delay to set your fade speed
   }
 }
-void ir() {
+void ir() { // IR handling
 
   if (irrecv.decode(&results)) {
-    if (results.value == IR_ON) {
+    if (results.value == IR_ON) { // When the power switch is pressed on the remote, we commute the global state of the lights, and call the state function
       isOn = !isOn;
       state();
     }
-    if (results.value == IR_DIY1 && isOn) {
+    if (results.value == IR_DIY1 && isOn) { // All of these are colors (RGBW), you can copy this function, change the "IR_DIYX" to the desired code (look in the variables), change the color and voilà
       newColor[0] = 255;
       newColor[1] = 255;
       newColor[2] = 255;
@@ -104,8 +104,8 @@ void ir() {
   }
 }
 
-void state() {
-  if (isOn) {
+void state() { // This function is a bit special, it makes the ON/OFF fade, but also turns HIGH and LOW the pin 13 for PSU's that can be enabled/disabled with a logic signal (for example: Xbox 360 PSU'S)
+  if (isOn) { // if you use this pin, make sure that the HIGH and LOW values are in the right place, as it might be inverted for you
     for (int i = 0; i <= 3; i++) {
       newColor[i] = backupColor[i];
     }
